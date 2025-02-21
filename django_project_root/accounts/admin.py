739615037e5b35c5039030290d3d54999aaf5d4d
Taras Bilan <taras.bilan@dci-student.org>
2025-02-21 11:19:09 +0100

    added models Mobiles, CustomUser to admin page

--- django_project_root/accounts/admin.py
+++ django_project_root/accounts/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import CustomUser
 # Register your models here.
+
+# add CustomUser model(table) to admin interface
+admin.site.register(CustomUser)
--- django_project_root/mobiles/admin.py
+++ django_project_root/mobiles/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Mobiles
 # Register your models here.
+
+# add Mobiles model(table) to admin interface
+admin.site.register(Mobiles)
