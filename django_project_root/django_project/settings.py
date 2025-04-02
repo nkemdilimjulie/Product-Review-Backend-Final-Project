09f1c5e7579319137867287cc6eef34787ef7c59
piet <pprotassow@posteo.de>
2025-04-02 12:15:15 +0200

    review: permission

--- django_project_root/django_project/settings.py
+++ django_project_root/django_project/settings.py
@@ -58,9 +58,11 @@ INSTALLED_APPS = [
     "reviews",
     # marketers related
     "marketers",
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -70,6 +72,9 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "allauth.account.middleware.AccountMiddleware",
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
+
 
 ROOT_URLCONF = 'django_project.urls'
 
--- django_project_root/mobiles/views.py
+++ django_project_root/mobiles/views.py
@@ -4,12 +4,12 @@ from .serializers import MobilesSerializer
 
 
 class MobileListView(generics.ListCreateAPIView):
-    permission_classes = (permissions.IsAdminUser,)
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
     queryset = Mobiles.objects.all()
     serializer_class = MobilesSerializer
 
 
 class MobileDetailView(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (permissions.IsAdminUser,)
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
     queryset = Mobiles.objects.all()
     serializer_class = MobilesSerializer
